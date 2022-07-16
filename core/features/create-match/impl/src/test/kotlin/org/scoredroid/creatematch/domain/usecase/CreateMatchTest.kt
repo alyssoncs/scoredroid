4808586aeccb1824e3ce0ba169ddc4183e53448a
alysson <alysson.cirilo@gmail.com>
2022-07-16 16:40:01 -0300

    rewrite test to use fake instead of stub

--- core/features/create-match/impl/src/test/kotlin/org/scoredroid/creatematch/domain/usecase/CreateMatchTest.kt
+++ core/features/create-match/impl/src/test/kotlin/org/scoredroid/creatematch/domain/usecase/CreateMatchTest.kt
@@ -4,18 +4,21 @@ import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
-import org.scoredroid.creatematch.testdouble.data.datasource.local.MatchLocalDataSourceStub
 import org.scoredroid.infra.dataaccess.repository.MatchRepository
+import org.scoredroid.infra.dataaccess.requestmodel.CreateMatchRepositoryRequest
+import org.scoredroid.infra.test.doubles.FakeMatchLocalDataSource
 
 @ExperimentalCoroutinesApi
 class CreateMatchTest {
-    private val matchLocalDataSourceStub = MatchLocalDataSourceStub()
+    private val matchLocalDataSourceStub = FakeMatchLocalDataSource()
     private val matchRepository = MatchRepository(matchLocalDataSourceStub)
     private val createMatch = CreateMatch(matchRepository)
 
     @Test
     fun `id provided by local data source`() = runTest {
-        matchLocalDataSourceStub.matchId = 2
+        repeat(2) {
+            matchLocalDataSourceStub.createMatch(CreateMatchRepositoryRequest(emptyList()))
+        }
 
         val match = createMatch()
 
