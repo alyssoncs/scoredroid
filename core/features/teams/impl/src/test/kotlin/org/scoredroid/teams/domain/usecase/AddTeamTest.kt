6d3f295521d805310478f8a7dbde77c2fe9822d2
alysson <alysson.cirilo@gmail.com>
2022-07-19 15:46:24 -0300

    refactor test

--- core/features/teams/impl/src/test/kotlin/org/scoredroid/teams/domain/usecase/AddTeamTest.kt
+++ core/features/teams/impl/src/test/kotlin/org/scoredroid/teams/domain/usecase/AddTeamTest.kt
@@ -8,16 +8,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.scoredroid.data.response.MatchResponse
-import org.scoredroid.infra.dataaccess.repository.MatchRepository
 import org.scoredroid.infra.dataaccess.requestmodel.CreateMatchRepositoryRequest
-import org.scoredroid.infra.test.doubles.FakeMatchLocalDataSource
+import org.scoredroid.infra.test.doubles.factories.repository.MatchRepositoryTestFactory
 import org.scoredroid.teams.domain.request.AddTeamRequest
 
 @ExperimentalCoroutinesApi
 class AddTeamTest {
 
-    private val fakeLocalDataSource = FakeMatchLocalDataSource()
-    private val repository = MatchRepository(fakeLocalDataSource)
+    private val repository = MatchRepositoryTestFactory.create()
     private val addTeam = AddTeam(repository)
 
     @Nested
