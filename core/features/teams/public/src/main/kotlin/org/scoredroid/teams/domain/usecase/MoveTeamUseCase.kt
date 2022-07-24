69b3ae0d7f83ef639a661ebf827e92f381c972f7
alysson <alysson.cirilo@gmail.com>
2022-07-24 20:47:50 -0300

    fix detekt issue

--- core/features/teams/public/src/main/kotlin/org/scoredroid/teams/domain/usecase/MoveTeamUseCase.kt
+++ core/features/teams/public/src/main/kotlin/org/scoredroid/teams/domain/usecase/MoveTeamUseCase.kt
@@ -1,7 +1,6 @@
 package org.scoredroid.teams.domain.usecase
 
 import org.scoredroid.data.response.MatchResponse
-import org.scoredroid.teams.domain.request.AddTeamRequest
 
 interface MoveTeamUseCase {
     suspend operator fun invoke(matchId: Long, teamAt: Int, moveTo: Int): Result<MatchResponse>
