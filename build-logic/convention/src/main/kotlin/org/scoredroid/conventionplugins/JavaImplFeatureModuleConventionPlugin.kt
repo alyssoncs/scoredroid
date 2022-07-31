d1133586fc4bab2afc100b3c56fd6f95d0fcb59c
alysson <alysson.cirilo@gmail.com>
2022-07-30 22:50:23 -0300

    create plugins

---
+++ build-logic/convention/src/main/kotlin/org/scoredroid/conventionplugins/JavaImplFeatureModuleConventionPlugin.kt
@@ -0,0 +1,11 @@
+package org.scoredroid.conventionplugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
+
+class JavaImplFeatureModuleConventionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.pluginManager.apply("org.scoredroid.java-module")
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/org/scoredroid/conventionplugins/JavaModuleConventionPlugin.kt
@@ -0,0 +1,26 @@
+package org.scoredroid.conventionplugins
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.internal.DefaultJavaPluginExtension
+import org.gradle.api.tasks.testing.Test
+
+class JavaModuleConventionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        val libs = project.extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+        with(project.pluginManager) {
+            apply("java-library")
+            apply(libs.findPlugin("kotlin.jvm").get().get().pluginId)
+            apply(libs.findPlugin("detekt").get().get().pluginId)
+            apply("org.scoredroid.java-module-junit5")
+        }
+
+        with(project.extensions.getByType(JavaPluginExtension::class.java)) {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+}
