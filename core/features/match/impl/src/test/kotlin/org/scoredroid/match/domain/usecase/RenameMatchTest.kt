f54dcb95b8c2ba75db8486af01f1abadeb15e99d
alysson <alysson.cirilo@gmail.com>
2022-08-25 09:32:31 -0300

    make test more resilient

--- core/features/match/impl/src/test/kotlin/org/scoredroid/match/domain/usecase/RenameMatchTest.kt
+++ core/features/match/impl/src/test/kotlin/org/scoredroid/match/domain/usecase/RenameMatchTest.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.scoredroid.data.response.MatchResponse
 import org.scoredroid.infra.test.fixtures.dataaccess.repository.MatchRepositoryFixtureFactory
 import kotlin.properties.Delegates
 
@@ -43,7 +44,15 @@ class RenameMatchTest {
         fun `return renamed match`() = runTest {
             val result = renameMatch(matchId, "specific name")
 
-            assertThat(result.getOrThrow().name).isEqualTo("specific name")
+            assertMatchWasRenamed(result, "specific name")
+        }
+
+        private suspend fun assertMatchWasRenamed(
+            result: Result<MatchResponse>,
+            matchName: String
+        ) {
+            assertThat(result.getOrThrow().name).isEqualTo(matchName)
+            assertThat(fixture.repository.getMatch(matchId)!!.name).isEqualTo(matchName)
         }
     }
 }
