045431e0068149372be9c8986964d6d8f9341164
alysson <alysson.cirilo@gmail.com>
2022-09-04 20:48:21 -0300

    migrate :core:features:match:impl to kotlin dsl

---
+++ core/features/match/impl/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.scoredroid.impl-feature")
+}
+
+dependencies {
+    implementation(projects.core.features.match.public)
+    implementation(projects.core.features.common.infra.public)
+    implementation(projects.core.features.common.utils)
+
+    implementation(libs.coroutines.core)
+
+    testImplementation(projects.core.features.common.infra.test)
+}
