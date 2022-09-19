21363e1be199a6eaf765bcc3c255165ab91a4f43
alysson <alysson.cirilo@gmail.com>
2022-09-19 10:03:32 -0300

    bump sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "org.scoredroid"
         minSdk = 25
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
