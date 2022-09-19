e761872823c6aa7c87f6b26c93991b49a22f0203
alysson <alysson.cirilo@gmail.com>
2022-09-19 12:19:02 -0300

    fix indentation

--- core/features/teams/impl/src/main/kotlin/org/scoredroid/teams/domain/usecase/MoveTeam.kt
+++ core/features/teams/impl/src/main/kotlin/org/scoredroid/teams/domain/usecase/MoveTeam.kt
@@ -11,13 +11,13 @@ class MoveTeam(
     override suspend fun invoke(matchId: Long, teamAt: Int, moveTo: Int): Result<MatchResponse> {
         val result = repository.moveTeam(matchId, teamAt, moveTo)
         val exception = result.exceptionOrNull()
-            when (exception) {
-                TeamOperationError.MatchNotFound -> UpdateTeamError.MatchNotFound
-                TeamOperationError.TeamNotFound -> UpdateTeamError.TeamNotFound
-                else -> null
-            }?.let {
-                return Result.failure(it)
-            }
+        when (exception) {
+            TeamOperationError.MatchNotFound -> UpdateTeamError.MatchNotFound
+            TeamOperationError.TeamNotFound -> UpdateTeamError.TeamNotFound
+            else -> null
+        }?.let {
+            return Result.failure(it)
+        }
         return result.map { it.toMatchResponse() }
     }
 }
