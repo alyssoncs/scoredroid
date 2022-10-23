e93eaf9cdaaaa8193cc36d77ec780bf37ad22c76
alysson <alysson.cirilo@gmail.com>
2022-10-23 00:49:34 -0300

    add dao

--- apps/android/common/infra/dependencies/impl/src/main/kotlin/org/scoredroid/infra/dataaccess/dao/MatchDao.kt
+++ apps/android/common/infra/dependencies/impl/src/main/kotlin/org/scoredroid/infra/dataaccess/dao/MatchDao.kt
@@ -1,9 +1,31 @@
 package org.scoredroid.infra.dataaccess.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import org.scoredroid.infra.dataaccess.entities.MatchEntity
+import org.scoredroid.infra.dataaccess.entities.TeamEntity
 
 @Dao
 interface MatchDao {
-    fun insertMatch(match: MatchEntity)
+    @Insert
+    fun insertMatch(match: MatchEntity, teams: List<TeamEntity>)
+
+    @Update
+    fun updateMatch(match: MatchEntity)
+
+    @Delete
+    fun deleteMatch(match: MatchEntity)
+
+    @Query(
+        """
+        SELECT * 
+        FROM `match` JOIN team 
+        ON `match`.id = match_id
+        WHERE `match`.id = :matchId
+        """
+    )
+    fun getMatch(matchId: Long): Map<MatchEntity, List<TeamEntity>>
 }
