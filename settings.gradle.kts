1b427a77d800d8b7fd302068dc42947760b43046
alysson <alysson.cirilo@gmail.com>
2022-10-30 18:03:19 -0300

    add app module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,7 @@ dependencyResolutionManagement {
 rootProject.name = "scoredroid"
 include(":app")
 include(":apps:android:common:infra:dependencies:impl")
+include(":apps:android:vanilla:app")
 include(":core:features:common:data")
 include(":core:features:common:domain")
 include(":core:features:common:infra:commonmodels")
