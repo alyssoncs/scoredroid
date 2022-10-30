7c48aca12bdb4e72e75e5b65d3c450a31844166f
alysson <alysson.cirilo@gmail.com>
2022-10-30 10:37:59 -0300

    add more info to the tests

--- build-logic/convention/src/main/kotlin/org/scoredroid/conventionplugins/JavaModuleJunit5ConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/org/scoredroid/conventionplugins/JavaModuleJunit5ConventionPlugin.kt
@@ -3,11 +3,20 @@ package org.scoredroid.conventionplugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 
 class JavaModuleJunit5ConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.tasks.withType(Test::class.java) { testTask ->
             testTask.useJUnitPlatform()
+            testTask.testLogging {
+                it.exceptionFormat = FULL
+                it.events = setOf(SKIPPED, PASSED, FAILED)
+                it.showStandardStreams = true
+            }
         }
     }
 }
