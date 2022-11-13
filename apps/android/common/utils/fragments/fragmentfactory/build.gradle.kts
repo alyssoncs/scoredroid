fa35e223d708bed0bbaafd2e96dc7b9cc85e056a
alysson <alysson.cirilo@gmail.com>
2022-11-13 13:23:44 -0300

    convert fragment factory to android lib plugin

--- apps/android/common/utils/fragments/fragmentfactory/build.gradle.kts
+++ apps/android/common/utils/fragments/fragmentfactory/build.gradle.kts
@@ -1,37 +1,10 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("org.scoredroid.android-lib")
     id("org.scoredroid.dagger")
 }
 
 android {
     namespace = "org.scoredroid.fragment.factory"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 25
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
