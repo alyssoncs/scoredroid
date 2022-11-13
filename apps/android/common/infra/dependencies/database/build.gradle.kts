7975053956b20b58c6652e7d6fc570ff8bc9f109
alysson <alysson.cirilo@gmail.com>
2022-11-13 13:27:12 -0300

    convert database to android lib plugin

--- apps/android/common/infra/dependencies/database/build.gradle.kts
+++ apps/android/common/infra/dependencies/database/build.gradle.kts
@@ -1,41 +1,16 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("org.scoredroid.android-lib")
     id("org.scoredroid.dagger")
     alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "org.scoredroid.infra.db"
-    compileSdk = 33
 
     defaultConfig {
-        minSdk = 25
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
         }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
 }
 
