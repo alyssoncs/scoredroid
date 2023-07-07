5a18317a770cd803b22fa98289bd5ea8d8484689
alysson <alysson.cirilo@gmail.com>
2023-07-07 19:37:39 -0300

    update dagger convention plugin

--- build-logic/convention/src/main/kotlin/org.scoredroid.dagger.gradle.kts
+++ build-logic/convention/src/main/kotlin/org.scoredroid.dagger.gradle.kts
@@ -1,3 +1,5 @@
+import gradle.kotlin.dsl.accessors._0a60746abe7e8186900dfa6c83859209.implementation
+import gradle.kotlin.dsl.accessors._8b7e56200444878463e419b6e3baca9b.kapt
 import org.scoredroid.utils.getLibrary
 import org.scoredroid.utils.versionCatalog
 
@@ -7,6 +9,6 @@ plugins {
 
 dependencies {
     val catalog = project.versionCatalog
-    add("implementation", catalog.getLibrary("dagger"))
-    add("kapt", catalog.getLibrary("dagger.compiler"))
+    implementation(catalog.getLibrary("dagger"))
+    kapt(catalog.getLibrary("dagger.compiler"))
 }
