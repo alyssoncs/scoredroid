248010336d46d6a4178836aea8e73fca311013b3
alysson <alysson.cirilo@gmail.com>
2023-07-07 19:26:14 -0300

    update unit test convention plugin

--- build-logic/convention/src/main/kotlin/org.scoredroid.unit-test.gradle.kts
+++ build-logic/convention/src/main/kotlin/org.scoredroid.unit-test.gradle.kts
@@ -1,9 +1,11 @@
+import gradle.kotlin.dsl.accessors._0a60746abe7e8186900dfa6c83859209.testImplementation
+import gradle.kotlin.dsl.accessors._0a60746abe7e8186900dfa6c83859209.testRuntimeOnly
 import org.scoredroid.utils.getBundle
 import org.scoredroid.utils.getLibrary
 import org.scoredroid.utils.versionCatalog
 
 dependencies {
     val catalog = project.versionCatalog
-    add("testRuntimeOnly", catalog.getLibrary("test.junit.engine"))
-    add("testImplementation", catalog.getBundle("unitTest"))
+    testRuntimeOnly(catalog.getLibrary("test.junit.engine"))
+    testImplementation(catalog.getBundle("unitTest"))
 }
