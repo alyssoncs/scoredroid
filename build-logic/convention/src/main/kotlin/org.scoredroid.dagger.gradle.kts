0b04751f1a64d363687a32d1e45dfd81621df979
alysson <alysson.cirilo@gmail.com>
2023-07-22 18:41:08 -0300

    change to a more robust configuration setup

--- build-logic/convention/src/main/kotlin/org.scoredroid.dagger.gradle.kts
+++ build-logic/convention/src/main/kotlin/org.scoredroid.dagger.gradle.kts
@@ -1,5 +1,3 @@
-import gradle.kotlin.dsl.accessors._0a60746abe7e8186900dfa6c83859209.implementation
-import gradle.kotlin.dsl.accessors._8b7e56200444878463e419b6e3baca9b.kapt
 import org.scoredroid.utils.getLibrary
 import org.scoredroid.utils.versionCatalog
 
@@ -9,6 +7,6 @@ plugins {
 
 dependencies {
     val catalog = project.versionCatalog
-    implementation(catalog.getLibrary("dagger"))
-    kapt(catalog.getLibrary("dagger.compiler"))
+    "implementation"(catalog.getLibrary("dagger"))
+    "kapt"(catalog.getLibrary("dagger.compiler"))
 }
