eb8071e2fbcf8eba1445d65b9f801893b90ade1e
alysson <alysson.cirilo@gmail.com>
2023-09-02 13:10:01 -0300

    make detekt happy

--- apps/android/vanilla/features/play/impl/src/main/kotlin/org/scoredroid/play/ui/screen/PlayScreen.kt
+++ apps/android/vanilla/features/play/impl/src/main/kotlin/org/scoredroid/play/ui/screen/PlayScreen.kt
@@ -5,12 +5,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import org.scoredroid.play.ui.viewmodel.PlayViewModel
 
 @Composable
-fun PlayScreen(viewModel: PlayViewModel) {
+fun PlayScreen(
+    viewModel: PlayViewModel,
+    modifier: Modifier = Modifier,
+) {
     val uiState by viewModel.uiState.collectAsState()
-    Button(onClick = {}) {
+    Button(onClick = {}, modifier) {
         Text(text = "$uiState")
     }
 }
