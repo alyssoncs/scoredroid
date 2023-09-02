10888ed7cc20c895f607317d1ed4896ce5dbdca8
alysson <alysson.cirilo@gmail.com>
2023-09-02 17:12:09 -0300

    use compose plugin

--- apps/android/vanilla/common/ui/components/build.gradle.kts
+++ apps/android/vanilla/common/ui/components/build.gradle.kts
@@ -1,23 +1,12 @@
 plugins {
     id("org.scoredroid.android-lib")
+    id("org.scoredroid.android-compose")
 }
 
 android {
     namespace = "org.scoredroid.ui.components"
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
 }
 
 dependencies {
     api(libs.immutable.collections)
-
-    implementation(libs.compose.material3)
-    implementation(libs.compose.ui.tooling.preview)
-    debugImplementation(libs.compose.ui.tooling)
 }
--- apps/android/vanilla/common/ui/theme/build.gradle.kts
+++ apps/android/vanilla/common/ui/theme/build.gradle.kts
@@ -1,23 +1,13 @@
 plugins {
     id("org.scoredroid.android-lib")
+    id("org.scoredroid.android-compose")
 }
 
 android {
     namespace = "org.scoredroid.ui.theme"
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
 }
 
 dependencies {
     libs.versions.compose
     implementation(libs.androidx.core.ktx)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.ui.tooling.preview)
-    debugImplementation(libs.compose.ui.tooling)
 }
