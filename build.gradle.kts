9720ade6a19fac7fab3ec8e54a04a6696ac79991
alysson <alysson.cirilo@gmail.com>
2023-09-08 11:37:39 -0300

    remove detekt from classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.detekt) apply false
     alias(libs.plugins.android.junit5) apply false
 }
 
