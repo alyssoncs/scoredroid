22600420b9463a08ecebd13cec71ecd6960a8512
alysson <alysson.cirilo@gmail.com>
2023-10-04 22:11:09 -0300

    remove unused import

--- apps/android/vanilla/features/history/impl/src/main/kotlin/org/scoredroid/history/ui/screen/MatchHistoryScreen.kt
+++ apps/android/vanilla/features/history/impl/src/main/kotlin/org/scoredroid/history/ui/screen/MatchHistoryScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
