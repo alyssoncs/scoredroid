ff517eee81485400a5b4d95c54c8e5b49b569b37
alysson <alysson.cirilo@gmail.com>
2023-10-10 22:26:33 -0300

    reduce number of deps

--- apps/android/vanilla/common/ui/tooling/build.gradle.kts
+++ apps/android/vanilla/common/ui/tooling/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("org.scoredroid.android-lib")
-    id("org.scoredroid.android-compose")
+    id("org.scoredroid.enable-compose")
 }
 
 android {
     namespace = "org.scoredroid.ui.tooling"
+}
+
+dependencies {
+    implementation(libs.compose.ui.tooling.preview)
 }
