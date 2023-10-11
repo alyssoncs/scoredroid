7582fe8b534121919d1d1a957890c750a6610d1b
alysson <alysson.cirilo@gmail.com>
2023-10-10 22:55:58 -0300

    bump gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
