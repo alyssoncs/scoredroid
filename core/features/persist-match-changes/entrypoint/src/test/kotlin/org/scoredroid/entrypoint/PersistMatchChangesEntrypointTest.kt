336434a4fe22abaa05490d832f7596ec3c5fac49
alysson <alysson.cirilo@gmail.com>
2023-12-03 20:26:14 -0300

    replace truth with kotest in :core:features:persist-match-changes:entrypoint

--- core/features/persist-match-changes/entrypoint/src/test/kotlin/org/scoredroid/entrypoint/PersistMatchChangesEntrypointTest.kt
+++ core/features/persist-match-changes/entrypoint/src/test/kotlin/org/scoredroid/entrypoint/PersistMatchChangesEntrypointTest.kt
@@ -1,6 +1,6 @@
 package org.scoredroid.entrypoint
 
-import com.google.common.truth.Truth
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.junit.jupiter.api.Test
 import org.scoredroid.infra.test.doubles.dataaccess.repository.FakePersistentMatchDataSource
 import org.scoredroid.usecase.ClearTransientMatchData
@@ -20,9 +20,9 @@ class PersistMatchChangesEntrypointTest {
         assertCorrectInstance<ClearTransientMatchData> { clearTransientMatchDataUseCase }
     }
 
-    private inline fun <reified T>assertCorrectInstance(
+    private inline fun <reified T : Any>assertCorrectInstance(
         dependency: PersistMatchChangesEntrypoint.() -> Any,
     ) {
-        Truth.assertThat(entrypoint.dependency()).isInstanceOf(T::class.java)
+        entrypoint.dependency().shouldBeInstanceOf<T>()
     }
 }
