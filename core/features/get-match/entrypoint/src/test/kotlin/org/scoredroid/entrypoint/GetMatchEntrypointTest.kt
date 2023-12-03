31616a76a3db5e6be2702105c903ce2c2abe6832
alysson <alysson.cirilo@gmail.com>
2023-12-03 20:21:19 -0300

    replace truth with kotest in :core:features:get-match:entrypoint

--- core/features/get-match/entrypoint/src/test/kotlin/org/scoredroid/entrypoint/GetMatchEntrypointTest.kt
+++ core/features/get-match/entrypoint/src/test/kotlin/org/scoredroid/entrypoint/GetMatchEntrypointTest.kt
@@ -1,6 +1,6 @@
 package org.scoredroid.entrypoint
 
-import com.google.common.truth.Truth
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.junit.jupiter.api.Test
 import org.scoredroid.infra.test.doubles.dataaccess.repository.FakePersistentMatchDataSource
 import org.scoredroid.usecase.GetMatchFlow
@@ -20,9 +20,9 @@ class GetMatchEntrypointTest {
         assertCorrectInstance<GetMatchesFlow> { getMatchesFlowUseCase }
     }
 
-    private inline fun <reified T>assertCorrectInstance(
+    private inline fun <reified T : Any>assertCorrectInstance(
         dependency: GetMatchEntrypoint.() -> Any,
     ) {
-        Truth.assertThat(entrypoint.dependency()).isInstanceOf(T::class.java)
+        entrypoint.dependency().shouldBeInstanceOf<T>()
     }
 }
