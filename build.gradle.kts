6d22b1054af38eebb2b1418ccc9351bb53543da3
alysson <alysson.cirilo@gmail.com>
2024-07-01 17:21:32 -0300

    fix warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
